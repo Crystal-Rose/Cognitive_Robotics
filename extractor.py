294bcf7aea0f8c7d6e2b656fc7e3eb5371238a86
Ivan <retired.inhausted@gmail.com>
2021-04-01 11:52:31 +0100

    OpenSMILE extraction code initiated

--- extractor.py
+++ extractor.py
@@ -3,6 +3,7 @@ import librosa.display
 import matplotlib.pyplot as plt#
 import seaborn
 import numpy as np
+import subprocess
 
 # Load audio
 signal, sr = librosa.load("audio.wav", sr=22050)
@@ -21,3 +22,23 @@ zcr = np.array(zcr).flatten()
 x = range(len(zcr))
 seaborn.lineplot(x=x, y=zcr)
 plt.show()
+
+def extractAudioFeatures(audio_path, config_path, output_path):
+    subprocess.call(
+        ["SMILExtract", "-C", config_path,
+            "-I", audio_path,
+            # "-lldcsvoutput", output_path
+            # "-frameModeFunctionalsConf", "openSMILE_conf/egemaps/FrameModeFunctionals.conf.inc",
+            "-O", output_path
+        ]
+    )
+
+'''
+Features by Alqudah:
+zero crossing rate, energy entropy, short time energy, spectral rolloff, spectral centroid and spectral flux
+
+SMILExtract -C "opensmile/alqudah.conf" -I "audio/1s.wav" -O "out.csv"
+'''
+
+if __name__ == "__main__":
+    pass
