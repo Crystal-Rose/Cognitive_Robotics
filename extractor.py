cf9acb4cffb4c97905e6bbc1c4042d3a9bfd22e3
Ivan <retired.inhausted@gmail.com>
2021-04-01 10:47:16 +0100

    Skeleton code for extraction and training

--- extractor.py
+++ extractor.py
@@ -3,6 +3,8 @@ import librosa.display
 import matplotlib.pyplot as plt#
 import seaborn
 import numpy as np
+from variables import ANNOTATIONS_PATH
+import json
 
 # Load audio
 signal, sr = librosa.load("audio.wav", sr=22050)
@@ -21,3 +23,17 @@ zcr = np.array(zcr).flatten()
 x = range(len(zcr))
 seaborn.lineplot(x=x, y=zcr)
 plt.show()
+
+def buildJSON():
+    obj = {}
+    data = list()
+    dataobj = {}
+    dataobj["class"] = 0
+    dataobj["label"] = 0
+    dataobj["features"] = []
+    data.append()
+    obj.append("data": data)
+    objstr = json.dumps(obj)
+    with open(ANNOTATIONS_PATH, "w") as f:
+        f.write(objstr)
+    
